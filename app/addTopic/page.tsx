11a7a4863acad2aac54b349fca6910e006aef856
Huends-Jhoebert <huendahuenda20@gmail.com>
2023-10-31 17:58:44 +0800

    Added e: React.FormEvent<HTMLFormElement>

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -3,67 +3,67 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 function AddTopic() {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!title || !description) {
-            alert("Title and descriptio are required");
-            return;
-        }
-        try {
-            const result = await fetch("http://localhost:3005/api/todo", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                },
-                body: JSON.stringify({ title, description }),
-            });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!title || !description) {
+      alert("Title and descriptio are required");
+      return;
+    }
+    try {
+      const result = await fetch("http://localhost:3005/api/todo", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
 
-            console.log(result);
+      console.log(result);
 
-            if (result.ok) {
-                router.refresh();
-                router.push("/");
-            } else {
-                throw new Error("Failed to create New Topic");
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-3 max-w-screen-xl m-auto py-4"
-        >
-            <input
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Topic Title"
-            />
+      if (result.ok) {
+        router.refresh();
+        router.push("/");
+      } else {
+        throw new Error("Failed to create New Topic");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-3 max-w-screen-xl m-auto py-4"
+    >
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
 
-            <input
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Topic Description"
-            />
+      <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
 
-            <button
-                type="submit"
-                className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
-            >
-                Add Topic
-            </button>
-        </form>
-    );
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Add Topic
+      </button>
+    </form>
+  );
 }
 
 export default AddTopic;
